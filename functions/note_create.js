595b5fda999eff23762dce462efca06080fef4e0
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2023-01-20 19:28:37 +0800

    initial commit

---
+++ functions/note_create.js
@@ -0,0 +1,10 @@
+exports = function({ query, headers, body}, response) {
+
+    const result = context.services
+      .get("mongodb-atlas")
+      .db("examples")
+      .collection("notes")
+      .insertOne(JSON.parse(body.text()));
+
+    return  result;
+};
---
+++ functions/note_get.js
@@ -0,0 +1,7 @@
+
+exports = function({ query, headers, body}, response) {
+    
+    const docs = context.services.get("mongodb-atlas").db("examples").collection("notes").find({}).toArray();
+    
+    return  docs;
+};
